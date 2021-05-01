493354bf2413c1e71e0e82d150559121d92934b4
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2021-04-30 20:04:50 -0500

    Add Banner to Home Screen component

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,11 +1,13 @@
 import styled from 'styled-components';
 
+import Banner from './Banner';
 import Nav from './Nav';
 
 export default function HomeScreen() {
   return (
     <HomeScreenContainer>
       <Nav />
+      <Banner />
     </HomeScreenContainer>
   );
 }
